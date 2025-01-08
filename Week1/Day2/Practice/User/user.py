654de9ce34abf4921a7536db8e55fbb673432926
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-08 17:31:40 +0100

    Add User class with constructor method to initialize attributes

--- Week1/Day2/Practice/User/user.py
+++ Week1/Day2/Practice/User/user.py
@@ -1,4 +1,6 @@
-class User:
+# define a class User
+class User: 
+    # constructor metghod to initialize the attribute of the class
     def __init__(self, first_name, last_name, email, age,):
         self.first_name=first_name
         self.last_name=last_name
