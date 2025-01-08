cbe1f68c0dc9b2ec3db1c5cd0aa9270c1b1008fb
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-08 15:19:14 +0100

    Add printInfo function to display key sizes and values from a dictionary with list values

--- Week1/Day1/Core/Nested Dictionaries & Lists/Nested Dictionaries & Lists.py
+++ Week1/Day1/Core/Nested Dictionaries & Lists/Nested Dictionaries & Lists.py
@@ -76,3 +76,18 @@ def iterateDictionary2(key_name, students):
 # test the function 
 iterateDictionary2('last_name', students)
 
+# 4.Iterate Through a Dictionary with List Values
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, 
+# and then prints the associated values within each key's list. For example:
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for key in some_dict:
+        print(f"{len(some_dict[key])}")
+        print(key.upper())
+        for value in some_dict[key]:
+            print(value)
+# test the function
+printInfo(dojo)
