020496d0c33674a4436e922541820e51aa41ce4b
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-10 19:10:37 +0100

    HTML Table

--- Week1/Day4/Practice/Understanding Routing/server.py
+++ Week1/Day4/Practice/Understanding Routing/server.py
@@ -1,27 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
-# localhost:5000/ - have it say "Hello World!"
+
+
 @app.route('/')
 def index():
     return 'Hello World'
-
-# localhost:5000/dojo - have it say "Dojo!"
 @app.route('/dojo')
 def dojo():
     return 'Dojo!'
-
-# localhost:5000/say/flask - have it say "Hi Flask!"
-
 @app.route('/say/<name>')
 def say(name):
     return "Hi  " + name + "!"
-
-# Create one url pattern and function that can handle the following examples 
-# (HINT: path variables are by default passed as strings. How might you handle a number?):
 @app.route('/repeat/<name>/<int:num>')
 def repeat(name, num):
     return f"hello, {name * num}"
 
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+if __name__=="__main__":  
+    app.run(debug=True)    
