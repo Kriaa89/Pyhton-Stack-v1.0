056f0fd7cf4b438522ee640eeaa5fc5548e3b357
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-16 15:17:18 +0100

    Add save method to UserCrud and create HTML templates for user management

--- wekk2/Day3/Practice/Users CRUD/flask_app/models/user.py
+++ wekk2/Day3/Practice/Users CRUD/flask_app/models/user.py
@@ -7,7 +7,7 @@ class UserCrud:
         self.last_name = data['last_name']
         self.email = data['email']
         self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+        self.updated_at = data['']
 # the get_all method will be used when we need to retrieve all the rows of the table 
     @classmethod
     def get_all(cls):
@@ -17,3 +17,9 @@ class UserCrud:
         for user in results:
             cruds.append(cls(user))
         return cruds
+# the save method will be used when we need to save a new friend to our database
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users(first_name, last_name, email, created_at, updated_at) VALUES(%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());"
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        return results
