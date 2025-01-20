7032c836bfd1d60b323ac2a57091560de81ba608
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-20 17:59:50 +0100

    Add Email model with database retrieval functionality

---
+++ wekk3/Day1/Practice/Email Validation with DB/flask_app/models/email.py
@@ -0,0 +1,16 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+class Email:
+    def __init__(self, data):
+        self.id = data['id']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM e;"
+        results = connectToMySQL(cls.DB).query_db(query)
+        dojos = []
+        for dojo in results:
+            dojos.append(cls(dojo))
+        return dojos
