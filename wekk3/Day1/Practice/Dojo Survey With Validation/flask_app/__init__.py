0f51f9573d6b90d488ac23b7322feec17854ab70
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-20 16:46:37 +0100

    Add session management and flash messaging to Dojo Survey app

--- wekk3/Day1/Practice/Dojo Survey With Validation/flask_app/__init__.py
+++ wekk3/Day1/Practice/Dojo Survey With Validation/flask_app/__init__.py
@@ -1,3 +1,4 @@
-from  flask import Flask, render_template, redirect, request
+from  flask import Flask, render_template, redirect, request, flash, session
 app = Flask(__name__)
+app.secret_key = 'keep it'
 
--- wekk3/Day1/Practice/Dojo Survey With Validation/server.py
+++ wekk3/Day1/Practice/Dojo Survey With Validation/server.py
@@ -3,5 +3,7 @@ from flask_app import app
 # here we will import the users.py file from the controllers folder
 from flask_app.controllers import dojos
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
