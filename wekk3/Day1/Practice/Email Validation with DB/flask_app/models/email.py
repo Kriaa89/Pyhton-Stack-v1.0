368bc566608df9e7a5dbcbcf7baba0986ef8d7cf
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-20 18:12:37 +0100

    Add email validation functionality and update database queries in Email model

--- wekk3/Day1/Practice/Email Validation with DB/flask_app/models/email.py
+++ wekk3/Day1/Practice/Email Validation with DB/flask_app/models/email.py
@@ -1,16 +1,37 @@
+import re
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 class Email:
+    DB = "Email_shcema"
     def __init__(self, data):
         self.id = data['id']
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        
+        
+        
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO validations (email) VALUES (%(email)s);"
+        return connectToMySQL(cls.DB).query_db(query, data)
+        
+        
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM e;"
+        query = "SELECT * FROM validations;"
         results = connectToMySQL(cls.DB).query_db(query)
-        dojos = []
-        for dojo in results:
-            dojos.append(cls(dojo))
-        return dojos
+        emails = []
+        for emails in results:
+            emails.append(cls(email))
+        return emails
+    @staticmethod
+    def validate_email(email):
+        is_valid = True
+        if not EMAIL_REGEX.match(email['email']):
+            flash("Invalid email address!")
+            is_valid = False
+        return is_valid
+        
+        
