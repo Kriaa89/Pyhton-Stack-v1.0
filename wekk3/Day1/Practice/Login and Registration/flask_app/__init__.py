169ef9083da40d8b64ec3f652c2265437b766776
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-21 16:55:09 +0100

    Remove deprecated files related to login and registration functionality

--- wekk3/Day1/Practice/Login and Registration/flask_app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-import re
-app=Flask(__name__)
-DATABASE = 'Login_schema'
-app.secret_key = "keep it secret"
-EMAIL_REGEX=re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
