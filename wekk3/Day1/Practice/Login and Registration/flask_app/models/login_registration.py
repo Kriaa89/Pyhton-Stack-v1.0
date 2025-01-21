55767c0a032796bc315d307a1c5afd3aad8c6dfa
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-21 14:16:06 +0100

    Add flask-bcrypt for password hashing and update server initialization

--- wekk3/Day1/Practice/Login and Registration/flask_app/models/login_registration.py
+++ wekk3/Day1/Practice/Login and Registration/flask_app/models/login_registration.py
@@ -2,6 +2,9 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app import app
 from flask_app import EMAIL_REGEX
+from flask_bcrypt import Bcrypt 
+bcrypt = Bcrypt(app) 
+
 
 class User:
     def __init__(self, data):
--- wekk3/Day1/Practice/Login and Registration/server.py
+++ wekk3/Day1/Practice/Login and Registration/server.py
@@ -1,1 +1,7 @@
 from flask_app import app
+from flask_app.controllers import login_registration
+
+
+
+if __name__ == "__main__":
+    app.run( debug = True )
