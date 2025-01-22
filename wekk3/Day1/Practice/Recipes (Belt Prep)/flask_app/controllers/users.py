d16f5d0f6f5faf838a08ef4aef2611c2057bf409
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-01-22 16:16:56 +0100

    Implement user registration and login functionality with password hashing; add login and registration templates

---
+++ wekk3/Day1/Practice/Recipes (Belt Prep)/flask_app/controllers/users.py
@@ -0,0 +1,42 @@
+from flask import render_template, redirect, request, session, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.reciep import Recipe
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not User.validate_registration(request.form):
+        return redirect('/')
+    # create the hash
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "password" : pw_hash
+    }
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect('/')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = {
+        "email": request.form['email']
+    }
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("Invalid Email/Password")
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    return redirect('/dashborde')
